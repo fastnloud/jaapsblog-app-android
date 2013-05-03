af77493958b3b895ce63f4723e82a36b2ecae279
jaap <jaap.vanturnhout@gmail.com>
2013-05-03 18:44:56 +0200

    Added Field mapping in PageActivity; so fetching data will be easier.

--- src/nl/jaapsblog/app/PageActivity.java
+++ src/nl/jaapsblog/app/PageActivity.java
@@ -13,13 +13,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PageActivity extends ListActivity implements Tasks {
 
     private ProgressDialog progressDialog;
     private Requester requester;
 
-    private ArrayList<String> values = new ArrayList<String>();
+    private ArrayList<Integer> fieldIds = new ArrayList<Integer>();
+    private String[] fieldNames;
+
+    private ArrayList<String> list = new ArrayList<String>();
     private ArrayAdapter<String> adapter;
 
     private JSONArray data;
@@ -30,7 +34,7 @@ public class PageActivity extends ListActivity implements Tasks {
         adapter = new ArrayAdapter<String> (
             this,
             android.R.layout.simple_list_item_1,
-            values // empty initially
+            list // empty initially
         );
 
         setListAdapter(adapter);
@@ -38,23 +42,32 @@ public class PageActivity extends ListActivity implements Tasks {
         // fetch data
         requester = new Requester(this);
         requester.execute(getString(R.string.url_page_index));
+
+        // set field ids.
+        fieldIds.addAll(
+            Arrays.asList(
+                R.id.id, R.id.title, R.id.label, R.id.url_string, R.id.route,
+                R.id.content, R.id.status, R.id.priority, R.id.meta_title,
+                R.id.meta_description, R.id.meta_keywords
+            )
+        );
+
+        // mapping for field ids.
+        fieldNames = new String[] {
+            "id", "title", "label", "url_string", "route", "content", "status",
+            "priority", "meta_title", "meta_description", "meta_keywords"
+        };
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         try {
-            JSONObject obj = data.getJSONObject(position);
-
             Intent intent = new Intent(getApplicationContext(), PageEditActivity.class);
+            intent.putIntegerArrayListExtra("fieldIds", fieldIds);
 
-            String extra[] = new String[] {
-                "id", "title", "label", "url_string", "route", "content", "status",
-                "priority", "meta_title", "meta_description", "meta_keywords"
-            };
-
-            intent.putExtra("extra", extra);
-            for(int i = 0; i < extra.length; ++i) {
-                intent.putExtra(extra[i], obj.get(extra[i]).toString());
+            JSONObject obj = data.getJSONObject(position);
+            for(int i = 0; i < fieldNames.length; ++i) {
+                intent.putExtra(fieldIds.get(i).toString(), obj.get(fieldNames[i]).toString());
             }
 
             startActivity(intent);
@@ -74,7 +87,7 @@ public class PageActivity extends ListActivity implements Tasks {
         try {
             data = requester.getDataParsed();
             for(int i = 0 ; i < data.length() ; i++){
-                values.add(data.getJSONObject(i).get("title").toString());
+                list.add(data.getJSONObject(i).get("title").toString());
             }
 
             // update list
--- src/nl/jaapsblog/app/PageEditActivity.java
+++ src/nl/jaapsblog/app/PageEditActivity.java
@@ -3,6 +3,7 @@ package nl.jaapsblog.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListAdapter;
@@ -13,33 +14,26 @@ import java.util.Arrays;
 
 public class PageEditActivity extends Form implements Tasks {
 
-    private ArrayList<Integer> fields = new ArrayList<Integer>();
-
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_edit);
 
-        if (getIntent().hasExtra("extra")) {
-            fields.addAll(
-                Arrays.asList(
-                    R.id.id,R.id.title, R.id.label, R.id.url_string, R.id.route,
-                    R.id.content, R.id.status, R.id.priority, R.id.meta_title,
-                    R.id.meta_description, R.id.meta_keywords
-                )
-            );
+        if (getIntent().hasExtra("fieldIds")) {
+            ArrayList<Integer> fieldIds = getIntent().getIntegerArrayListExtra("fieldIds");
 
-            String extra[] = getIntent().getStringArrayExtra("extra");
-            for (int i = 0; i < extra.length; ++i) {
-                if (extra[i].equals("priority")) {
-                    createSpinner(fields.get(i), "priority", getIntent().getStringExtra(extra[i]));
-                } else if (extra[i].equals("status")) {
+            for (int i = 0; i < fieldIds.size(); ++i) {
+                Integer id = fieldIds.get(i);
+                String value = getIntent().getStringExtra(fieldIds.get(i).toString());
+
+                if (6 == i) { // status
                     ArrayList values = new ArrayList<String>();
                     values.addAll(Arrays.asList("online", "online-not-in-menu", "offline"));
 
-                    createSpinner(fields.get(i), values, getIntent().getStringExtra(extra[i]));
+                    createSpinner(id, values, value);
+                } else if (7 == i) { // priority
+                    createSpinner(id, "priority", value);
                 } else {
-                    EditText field = (EditText) findViewById(fields.get(i));
-                    field.setText(getIntent().getStringExtra(extra[i]));
+                    createTextField(id, value);
                 }
             }
         }
