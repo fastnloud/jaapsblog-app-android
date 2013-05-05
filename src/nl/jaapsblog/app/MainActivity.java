08bcb213714b7fe2b9f7acdec8e332953d6a3574
jaap <jaap.vanturnhout@gmail.com>
2013-05-05 16:44:48 +0200

    Added Logout button to Activity option menu.

--- src/nl/jaapsblog/app/MainActivity.java
+++ src/nl/jaapsblog/app/MainActivity.java
@@ -3,6 +3,8 @@ package nl.jaapsblog.app;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -23,6 +25,22 @@ public class MainActivity extends ListActivity {
         );
 
         setListAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (0 == item.getOrder()) {
+            Intent intent = new Intent(this, KillActivity.class);
+            startActivity(intent);
+        }
+
+        return false;
     }
 
     @Override
