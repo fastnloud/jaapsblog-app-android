f86fd504446d51279b96d6b46516ef2996073cde
jaap <jaap.vanturnhout@gmail.com>
2013-05-06 21:09:17 +0200

    Added title to Context menu.

--- src/nl/jaapsblog/app/ListCreator.java
+++ src/nl/jaapsblog/app/ListCreator.java
@@ -9,10 +9,7 @@ import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.*;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
@@ -163,6 +160,7 @@ public class ListCreator extends ListActivity implements Tasks  {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Action Menu");
         getMenuInflater().inflate(R.menu.list_context, menu);
     }
 
